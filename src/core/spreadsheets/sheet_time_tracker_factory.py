32a2d063990c8651e5cad7095b8e0eaecdfc7f52
Wisteca <thebastian1111@gmail.com>
2025-05-17 11:57:56 +0200

    feat: time tracker factory skeleton

---
+++ src/core/spreadsheets/sheet_time_tracker_factory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+File: sheet_time_tracker_factory.py
+Author: Bastian Cerf
+Date: 17/05/2025
+Description: 
+    Implementation of the factory interface defined in `time_tracker_factory.py`.
+    This implementation works with spreadsheets file and build SheetTimeTracker
+    instances.
+
+Company: Mecacerf SA
+Website: http://mecacerf.ch
+Contact: info@mecacerf.ch
+"""
+
---
+++ src/core/time_tracker_factory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+File: time_tracker_factory.py
+Author: Bastian Cerf
+Date: 17/05/2025
+Description: 
+    Factory for creating instances of employee's time trackers.
+    The actual time tracker implementation in use is configured at initialization
+    and hidden to the user. 
+
+Company: Mecacerf SA
+Website: http://mecacerf.ch
+Contact: info@mecacerf.ch
+"""
+
