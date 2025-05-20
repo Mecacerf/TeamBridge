e8abb07411f090559f85b4aa822e53bf4ade81f6
Wisteca <thebastian1111@gmail.com>
2025-05-20 23:29:37 +0200

    feat: sheet time tracker factory

--- src/core/spreadsheets/sheet_time_tracker_factory.py
+++ src/core/spreadsheets/sheet_time_tracker_factory.py
@@ -4,12 +4,44 @@ File: sheet_time_tracker_factory.py
 Author: Bastian Cerf
 Date: 17/05/2025
 Description: 
-    Implementation of the factory interface defined in `time_tracker_factory.py`.
-    This implementation works with spreadsheets file and build SheetTimeTracker
-    instances.
+    Concrete implementation of the abstract factory defined in 
+    `time_tracker_factory.py`. This implementation builds
+    `SheetTimeTracker` instances.
 
 Company: Mecacerf SA
 Website: http://mecacerf.ch
 Contact: info@mecacerf.ch
 """
 
+# Standard libraries
+import datetime as dt
+from typing import Optional
+
+# Internal libraries
+from core.time_tracker import BaseTimeTracker
+from core.time_tracker_factory import TimeTrackerFactory
+from .sheet_time_tracker import SheetTimeTracker
+from .sheets_repository import *
+
+class SheetTimeTrackerFactory(TimeTrackerFactory):
+    """
+    Concrete implementation of the abstract factory defined in 
+    `time_tracker_factory.py`. This implementation builds
+    `SheetTimeTracker` instances.
+    """
+
+    def __init__(self, repository_path: str):
+        """
+        Initialize the factory.
+
+        Args:
+            repository_path (str): Path to the spreadsheets repository.
+        """
+        configure(remote_repository=repository_path)
+
+    def create(self, employee_id: str, 
+               date: Optional[dt.date] = None) -> BaseTimeTracker:
+        return SheetTimeTracker(employee_id, date)
+
+    def list_employee_ids(self) -> list[str]:
+        return list_employee_ids()
--- src/core/time_tracker_factory.py
+++ src/core/time_tracker_factory.py
@@ -4,12 +4,56 @@ File: time_tracker_factory.py
 Author: Bastian Cerf
 Date: 17/05/2025
 Description: 
-    Factory for creating instances of employee's time trackers.
-    The actual time tracker implementation in use is configured at initialization
-    and hidden to the user. 
+    Abstract factory for creating time tracker instances. The concrete type
+    of the time tracker is defined by the subclass implementation.
 
 Company: Mecacerf SA
 Website: http://mecacerf.ch
 Contact: info@mecacerf.ch
 """
 
+# Standard libraries
+import datetime as dt
+from typing import Optional
+from abc import ABC, abstractmethod
+
+# Internal libraries
+from .time_tracker import BaseTimeTracker
+
+class TimeTrackerFactory(ABC):
+    """
+    Abstract factory for creating time tracker instances. The concrete type
+    of the time tracker is defined by the subclass implementation.
+    """
+
+    @abstractmethod
+    def create(self, employee_id: str, 
+               date: Optional[dt.date] = None) -> BaseTimeTracker:
+        """
+        Create a time tracker for the given employee ID. An optional date can
+        be provided; if omitted, a default such as January 1st may be used,
+        depending on the implementation.
+
+        Args:
+            employee_id (str): Unique identifier for the employee.
+            date (Optional[datetime.date]): Optional current date pointer. Defaults
+                to first of January.
+
+        Returns:
+            BaseTimeTracker: An instance of a time tracker for the specified employee.
+
+        Raises:
+            TimeTrackerOpenException: Raised if the time tracker cannot be opened.
+                See the underlying exception for details.
+        """
+        pass
+
+    @abstractmethod
+    def list_employee_ids(self) -> list[str]:
+        """
+        List all registered employee IDs.
+
+        Returns:
+            list[str]: A list of employee identifiers.
+        """
+        pass
