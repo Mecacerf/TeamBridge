45a5fff401018e58dba2d26ddfc8d2979d5b16c5
Wisteca <thebastian1111@gmail.com>
2025-07-20 21:59:59 +0200

    feat: simple attendance validator implementation

---
+++ src/core/attendance/simple_attendance_validator.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+File: simple_attendance_validator.py
+Author: Bastian Cerf
+Date: 20/07/2025
+Description:
+    Simple implementation of base classes provided in
+    `attendance_validator.py`.
+
+Company: Mecacerf SA
+Website: http://mecacerf.ch
+Contact: info@mecacerf.ch
+"""
+
+# Standard libraries
+import logging
+import datetime as dt
+
+# Internal libraries
+from .attendance_validator import AttendanceChecker, AttendanceValidator
+from core.time_tracker import TimeTracker
+
+logger = logging.getLogger(__name__)
+
+
+class ContinuousWorkChecker(AttendanceChecker):
+    """ """
+
+    ERROR_ID = 10
+
+    def __init__(self):
+        super().__init__(self.ERROR_ID)
+
+    def check_date(self, tracker: TimeTracker, date: dt.date) -> bool:
+        """ """
+        events = tracker.get_clocks(date)
+        max_time = tracker.max_continuous_work_time
+
+        evt_pairs = [(e1, e2) for e1, e2 in zip(events, events[1:]) if e1 and e2]
+        return any(self.__delta(e1.time, e2.time) > max_time for e1, e2 in evt_pairs)
+
+    def __delta(self, t1: dt.time, t2: dt.time) -> dt.timedelta:
+        t1_delta = dt.timedelta(hours=t1.hour, minutes=t1.minute)
+        t2_delta = dt.timedelta(hours=t2.hour, minutes=t2.minute)
+        return max(dt.timedelta(0), t2_delta - t1_delta)
+
+
+class ClockSequenceChecker(AttendanceChecker):
+    """ """
+
+    ERROR_ID = 40
+
+    def __init__(self):
+        super().__init__(self.ERROR_ID)
+
+    def check_date(self, tracker: TimeTracker, date: dt.date) -> bool:
+        """ """
+        events = [evt for evt in tracker.get_clocks(date) if evt is not None]
+        return any(e1.time >= e2.time for e1, e2 in zip(events, events[1:]))
+
+
+class SimpleAttendanceValidator(AttendanceValidator):
+    """ """
+
+    def __init__(self):
+        super().__init__([ContinuousWorkChecker(), ClockSequenceChecker()])
